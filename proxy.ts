d277144447fe5a54aeb4946361ba1f0f61651423
Ihossein <www.hossein.shams77@gmail.com>
2026-06-09 03:26:52 +0430

    export mathcer middleware.ts

--- proxy.ts
+++ proxy.ts
@@ -78,9 +78,3 @@ export async function proxy(request: NextRequest) {
   return NextResponse.next();
 }
 
-// تنظیمات middleware
-export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
-};
