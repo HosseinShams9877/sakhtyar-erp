cfc812bb84b630a2786ca50528ce77d964270bd8
Ihossein <www.hossein.shams77@gmail.com>
2026-06-12 17:45:08 +0430

    fix api profile2

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -23,7 +23,7 @@ export async function GET() {
     }
 
     const user = await db.user.findUnique({
-      where: { email: session.user.email },
+      where: { id: session.user.id },
       select: {
         avatar: true,
       },
